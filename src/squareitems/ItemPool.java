8b4062a604a5c14bea8289863ef438a25d83e0ea
Ryxali <Ryxali@gmail.com>
2013-03-24 21:18:36 +0100

    As requested sire!

--- src/squareitems/ItemPool.java
+++ src/squareitems/ItemPool.java
@@ -3,6 +3,11 @@ package squareitems;
  * This class reads from an item table file and
  * stores them here. These items can be accessed
  * by name and will reference an Item object.
+ * <p>
+ * Item table format:<br />
+ * ITEM_NAME; IMAGE_REF; ITEM_DESC;
+ * </p>
+ * 
  * @author Niklas L
  * @see squareitems.Item
  * @see squareitems.LootPile
